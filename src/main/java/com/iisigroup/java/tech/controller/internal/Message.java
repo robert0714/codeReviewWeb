7d0194612334bc4cc9d153961c954be48b586994
robert <robert0714@hotmail.com>
2015-09-07 17:00:18 +0800

    upload function test

---
+++ src/main/java/com/iisigroup/java/tech/controller/internal/Message.java
@@ -0,0 +1,19 @@
+package com.iisigroup.java.tech.controller.internal;
+
+public class Message {
+    private String type;
+    private String message;
+
+    public Message(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
